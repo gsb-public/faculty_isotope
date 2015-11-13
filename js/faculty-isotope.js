cc6f7a952f89e972b34edb638fe586095b4699c7
Adam Moore <redndahead@gmail.com>
2015-11-12 16:35:15 -0800

    Add filters.

--- js/faculty-isotope.js
+++ js/faculty-isotope.js
@@ -1,38 +1,82 @@
 (function ($) {
   Drupal.behaviors.facultyIsotope = {
     attach: function (context, settings) {
-      $grid = $('#faculty-isotope').isotope({
+      $grid = $('.faculty-list').isotope({
         itemSelector: '.views-row',
-        layoutMode: 'fitRows'
+        layoutMode: 'fitRows',
+        onLayout: function() {
+          $(window).trigger("scroll");
+        }
       });
 
-      $('#all').click(function(e) {
-        e.preventDefault();
-        $grid.isotope({ filter: '*' });
+      var $imgs = $(".faculty-list img");
+
+      $imgs.lazyload({
+        failure_limit: Math.max($imgs.length - 1, 0)
       });
 
-      $('#filter-economics').click(function(e) {
-        e.preventDefault();
-        $grid.isotope({ filter: '.economics' })
+      Drupal.settings.facultyIsotope = {};
+      Drupal.settings.facultyIsotope.searchResults = '';
+
+
+      $('.views-widget-filter-secondary input').click(function(e) {
+        Drupal.facultyIsotope.update();
       });
 
-      $('#filter-search').click(function(e) {
+      $('#edit-submit-faculty-filters').click(function(e) {
         e.preventDefault();
 
-        $.getJSON("/faculty-search/house", function(data) {
-          results = data;
-          $grid.isotope({filter: function() {
-            var nid = $(this).data('nid');
+        var text = $('#edit-search').val();
 
-            if ($.inArray(nid.toString(), results) == -1) {
-              return false;
-            }
-            else {
-              return true;
-            }
-          }});
+        $.getJSON("/faculty-search/" + text, function(data) {
+          Drupal.settings.facultyIsotope.searchResults = data;
+          Drupal.facultyIsotope.update();
         });
       });
     }
   };
+
+  Drupal.facultyIsotope = Drupal.facultyIsotope || {};
+
+  Drupal.facultyIsotope.update = function() {
+    $grid.isotope({filter: function() {
+      $this = $(this);
+      academicAreaTest = false;
+      if ($('.academic-areas input:checked').length) {
+        $('.academic-areas input:checked').each(function() {
+          if ($this.hasClass($(this).val())) {
+            academicAreaTest = true;
+          }
+        });
+      }
+      else {
+        academicAreaTest = true;
+      }
+
+      centerTest = false;
+      if ($('.centers input:checked').length) {
+        $('.centers input:checked').each(function() {
+          if ($this.hasClass($(this).val())) {
+            centerTest = true;
+          }
+        });
+      }
+      else {
+        centerTest = true;
+      }
+
+      var nid = $(this).data('nid');
+      searchTest = true;
+      if (Drupal.settings.facultyIsotope.searchResults.length && $.inArray(nid.toString(), Drupal.settings.facultyIsotope.searchResults) == -1) {
+        searchTest = false;
+      }
+
+      console.log(academicAreaTest);
+      console.log(centerTest);
+      console.log(searchTest);
+
+      return academicAreaTest && centerTest && searchTest;
+    }});
+
+  }
 })(jQuery);
--- templates/faculty-isotope-wrapper.tpl.php
+++ templates/faculty-isotope-wrapper.tpl.php
@@ -1,6 +1,61 @@
-<a id="all" href="#">All</a> <a id="filter-economics" href="#">Economics</a> <a id="filter-search" href="#">Search House</a>
 <div id="faculty-isotope" class="view-content pane-faculty-filters-faculty-list">
   <div class="view-faculty-filters">
-    <?php print $items; ?>
+    <div class="filter-group">
+      <div class="views-exposed-search-wrap">
+        <div id="edit-search-wrapper">
+          <div class="form-item form-item-search">
+            <input class="form-text" placeholder="search by name, research interests, or other keywords" type="text" id="edit-search" name="search" value="" size="30" maxlength="128">
+          </div>
+        </div>
+        <div class="views-submit-button">
+          <input type="submit" id="edit-submit-faculty-filters" />
+        </div>
+      </div>
+      <div class="views-widget-filter-secondary">
+        <fieldset class="collapsible form-wrapper collapse-processed" id="edit-secondary">
+          <legend>
+            <span class="fieldset-legend">
+              <a class="fieldset-title" href="#">
+                <span class="fieldset-legend-prefix element-invisible">Hide</span> Narrow your results
+              </a>
+              <span class="summary"></span>
+            </span>
+          </legend>
+          <div class="fieldset-wrapper" style="overflow: hidden;">
+            <div class="bef-secondary-options">
+              <div class="form-item form-type-select academic-areas">
+                <label for="edit-term-node-tid-depth">Academic Area </label>
+                <div class="form-checkboxes bef-select-as-checkboxes bef-required-filter-processed">
+                  <div class="bef-checkboxes">
+                    <?php foreach ($academic_areas as $academic_area_key => $academic_area_value): ?>
+                      <div class="form-item">
+                        <input type="checkbox" name="<?php print $academic_area_key; ?>" value="<?php print $academic_area_key; ?>">
+                        <label class="option"><?php print $academic_area_value; ?></label>
+                      </div>
+                    <?php endforeach; ?>
+                  </div>
+                </div>
+              </div>
+              <div class="form-item form-type-select centers">
+                <label for="edit-term-node-tid-depth-1">Centers, Initiatives, and Institutes </label>
+                <div class="form-checkboxes bef-select-as-checkboxes bef-required-filter-processed">
+                  <div class="bef-checkboxes">
+                    <?php foreach ($centers as $center_key => $center_value): ?>
+                      <div class="form-item">
+                        <input type="checkbox" name="<?php print $center_key; ?>" value="<?php print $center_key; ?>">
+                        <label class="option"><?php print $center_value; ?></label>
+                      </div>
+                    <?php endforeach; ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+    </div>
+    <div class="faculty-list" style="clear: both;">
+      <?php print $items; ?>
+    </div>
   </div>
 </div>
